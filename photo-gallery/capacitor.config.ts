cf68847c955a6135b1db869caba2c7a801664eaa
Naza29 <125493858+Naza29@users.noreply.github.com>
2023-11-29 16:35:24 +0000

    avance

---
+++ photo-gallery/capacitor.config.ts
@@ -0,0 +1,16 @@
+import { CapacitorConfig } from '@capacitor/cli';
+import { isPlatform } from '@ionic/vue';
+import { Capacitor } from '@capacitor/core';
+ 
+ 
+ 
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'photo-gallery',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+ 
+export default config;
